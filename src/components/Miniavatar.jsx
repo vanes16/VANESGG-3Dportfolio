12b1e726ac10647045d1e2ed296169edc62d8e36
Alessandro Fonso Albert Dequeque <110099784+vanes16@users.noreply.github.com>
2025-01-17 16:10:39 +0700

    Update Miniavatar.jsx

--- src/components/Miniavatar.jsx
+++ src/components/Miniavatar.jsx
@@ -1,46 +1,13 @@
 import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
 import { Suspense, useEffect, useRef } from "react";
 import { GLTFExporter } from "three-stdlib";
-import { pb, useConfiguratorStore } from "../store";
 import { Asset } from "./Asset";
 
 export const Miniavatar = ({ ...props }) => {
   const group = useRef();
   const { nodes } = useGLTF("/mini model/Mini model.glb");
   const { animations } = useFBX("/mini model/Idle.fbx");
-  const customization = useConfiguratorStore((state) => state.customization);
   const { actions } = useAnimations(animations, group);
-  const setDownload = useConfiguratorStore((state) => state.setDownload);
-
-  useEffect(() => {
-    function download() {
-      const exporter = new GLTFExporter();
-      exporter.parse(
-        group.current,
-        function (result) {
-          save(
-            new Blob([result], { type: "application/octet-stream" }),
-            `avatar_${+new Date()}.glb`
-          );
-        },
-        function (error) {
-          console.error(error);
-        },
-        { binary: true }
-      );
-    }
-
-    const link = document.createElement("a");
-    link.style.display = "none";
-    document.body.appendChild(link); // Firefox workaround, see #6594
-
-    function save(blob, filename) {
-      link.href = URL.createObjectURL(blob);
-      link.download = filename;
-      link.click();
-    }
-    setDownload(download);
-  }, []);
 
   useEffect(() => {
     actions["mixamo.com"]?.play();
@@ -50,20 +17,6 @@ export const Miniavatar = ({ ...props }) => {
       <group name="Scene">
         <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
           <primitive object={nodes.mixamorigHips} />
-          {Object.keys(customization).map(
-            (key) =>
-              customization[key]?.asset?.url && (
-                <Suspense key={customization[key].asset.id}>
-                  <Asset
-                    url={pb.files.getUrl(
-                      customization[key].asset,
-                      customization[key].asset.url
-                    )}
-                    skeleton={nodes.Plane.skeleton}
-                  />
-                </Suspense>
-              )
-          )}
         </group>
       </group>
     </group>
