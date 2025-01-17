794bd4ac7e54374698244d7a014b5b2a2210de2c
Alessandro Fonso Albert Dequeque <110099784+vanes16@users.noreply.github.com>
2025-01-17 19:29:38 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience"; 
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Experience } from "./components/Experience"; 
 import { UI } from "./components/UI"; 
 
 function App() {
   return (
     <>
-      <UI />
-      <Canvas gl={{
-          preserveDrawingBuffer: true,
-        }} shadows
+      <UI /> 
+      <Canvas
+        camera={{
+          position: [-1, 1, 5],
+          fov: 45,
+        }}
+        shadows
       >
         <color attach="background" args={["#130f30"]} />
         <fog attach="fog" args={["#130f30", 10, 40]} />
         <group position-y={-1}>
-          <Experience />
-        </group>
+          <Experience /> 
+        </group> 
         <EffectComposer>
           <Bloom mipmapBlur luminanceThreshold={1.2} intensity={1.2} />
         </EffectComposer>
