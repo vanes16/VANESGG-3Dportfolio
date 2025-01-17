7667e22b9c54fc9e7de02519d4dc38e9d3501f40
Alessandro Fonso Albert Dequeque <110099784+vanes16@users.noreply.github.com>
2025-01-17 16:24:55 +0700

    Update UI.jsx

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -5,11 +5,8 @@ export const UI = () => {
     <main className="pointer-events-none fixed z-10 inset-0 p-10">
       <div className="mx-auto h-full max-w-screen-xl w-full flex flex-col justify-between">
         <div className="flex justify-between items-center">
-          <a
-            className="pointer-events-auto"
-            href="https://lessons.wawasensei.dev/courses/react-three-fiber"
-          >
-            <img className="w-20" src="/images/wawasensei-white.png" />
+          <a className="pointer-events-auto" href="https://lessons.wawasensei.dev/courses/react-three-fiber">
+            VANESGG
           </a>
         </div>
         <div className="flex flex-col gap-6">
