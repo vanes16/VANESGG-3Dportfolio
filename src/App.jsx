714b2439f8804a409349a7968aecbc074da08208
Alessandro Fonso Albert Dequeque <110099784+vanes16@users.noreply.github.com>
2025-01-17 19:20:27 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience"; 
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { UI } from "./components/UI"; 
+import { Leva } from "leva";
 
 function App() {
   return (
     <>
-      <UI /> 
+      <Leva hidden />
+      <UI />
       <Canvas
-        camera={{
-          position: [-1, 1, 5],
-          fov: 45,
+        gl={{
+          preserveDrawingBuffer: true,
         }}
         shadows
       >
-        <color attach="background" args={["#555"]} />
-        <fog attach="fog" args={["#555", 15, 25]} />
+        <color attach="background" args={["#130f30"]} />
+        <fog attach="fog" args={["#130f30", 10, 40]} />
         <group position-y={-1}>
-          <Experience /> 
-        </group> 
+          <Experience />
+        </group>
+        <EffectComposer>
+          <Bloom mipmapBlur luminanceThreshold={1.2} intensity={1.2} />
+        </EffectComposer>
       </Canvas>
     </>
   );
