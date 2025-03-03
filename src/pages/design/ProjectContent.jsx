6c76678bca420b7630c174b59956f4f65778e84a
Alessandro Fonso Albert Dequeque <110099784+vanes16@users.noreply.github.com>
2025-03-03 09:54:56 +0700

    Add files via upload
    Update style

--- src/pages/design/ProjectContent.jsx
+++ src/pages/design/ProjectContent.jsx
@@ -6,16 +6,16 @@ const ProjectContent = () => {
         <img
           alt="Cyberpunk 2077 Patch 2.21 image with characters in a car"
           className="w-full h-[300px] object-cover"
-          src="https://storage.googleapis.com/a1aa/image/Cilkm8jBH62P1Yy8LrHGQnkqm_W2jwOPHHtHFaq2bBk.jpg"
+          src="/project/Bank.png"
         />
         <div className="absolute top-3 left-3 bg-yellow-500 text-black px-2 py-1 text-xs font-bold">
-          PATCH 2.21
+          CHATBOT FOR CUSTOMERS
         </div>
         <div className="absolute bottom-3 left-3 text-[#ff003c] text-2xl font-bold">
-          PATCH 2.21
+          CHATBOT USING TENSORFLOW PYTHON
         </div>
         <div className="absolute top-3 right-3 text-gray-400 text-xs">
-          23.01.2025
+          Final Project For Graduate
         </div>
       </div>
 
@@ -23,26 +23,26 @@ const ProjectContent = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 cyber-glitch-2">
         {[
           {
-            src: "https://storage.googleapis.com/a1aa/image/eqqKK7iFj3Mt3va89jt1CvKZCFRfnELGGxI4ztJzGlI.jpg",
-            label: "PHOTO MODE CHALLENGE",
-            text: "TAKE YOUR SHOT IN PHOTO MODE",
+            src: "/project/Virtualreality.png",
+            label: "VIRTUAL REALITY",
+            text: "Virtual reality using Unity",
             bg: "bg-yellow-500",
           },
           {
-            src: "https://storage.googleapis.com/a1aa/image/mqrC35SSNS_EU064-SEOSTXaf5RjCxGWKXgL13rGdcI.jpg",
-            label: "UPDATE 2.2 IS LIVE!",
-            bg: "bg-gray-800",
+            src: "/project/Bomberman.png",
+            label: "BOMBERMAN GAME",
+            bg: "bg-gray-300",
           },
           {
-            src: "https://storage.googleapis.com/a1aa/image/1D2PC_lvUW8ZJK9iQpM9D3EEWx5NNBlpk8TtP9H1AWE.jpg",
-            label: "@REDSTREAMS",
-            text: "REDSTREAMS – UPDATE 2.2",
+            src: "/project/Iot.png",
+            label: "INTERNET OF THINGS",
+            text: "Remote Control Car",
             bg: "bg-red-600",
           },
           {
-            src: "https://storage.googleapis.com/a1aa/image/1Ge5_VBzwrKUFJgIJZ5ExithtTKYYzui-x4zxtWi7Qc.jpg",
-            label: "COMING TO MACS",
-            text: "JUST ANNOUNCED – CYBERPUNK",
+            src: "/project/Movierec.png",
+            label: "MOVIE RECOMMENDATION WEBSITE",
+            text: "Website for movie using API",
             bg: "bg-yellow-500",
           },
         ].map((item, index) => (
@@ -52,13 +52,14 @@ const ProjectContent = () => {
               className="w-full h-[180px] object-cover"
               src={item.src}
             />
+            <div className="absolute inset-0 bg-black bg-opacity-20"></div>
             <div
               className={`absolute top-2 left-2 ${item.bg} text-black px-2 py-1 text-xs font-bold`}
             >
               {item.label}
             </div>
             {item.text && (
-              <div className="absolute bottom-2 left-2 text-white text-sm font-semibold">
+              <div className="absolute bottom-2 left-2 p-1 text-white text-sm font-semibold">
                 {item.text}
               </div>
             )}
